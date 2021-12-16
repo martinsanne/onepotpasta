caae02c862e809a4c7b82348121ccbb9ebe1f888
Martin Sanne Kristiansen <martin@heydays.no>
2021-12-16 12:25:39 +0100

    add checkboxes

--- pages/index.js
+++ pages/index.js
@@ -11,44 +11,79 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <h1 className={styles.title}>Skikkelig digg one-pot pasta</h1>
+        <hr />
         <h2 className={styles.title}>Ingredienser</h2>
         <ul className={styles.list}>
           <li data-uid={0}>
-            <span>200</span> <span>g</span> <span>spaghetti</span>{' '}
-            <span>gjerne fullkorn</span>
+            <label>
+              <input type="checkbox" />
+              <span>200</span> <span>g</span> <span>spaghetti</span>{' '}
+              <span>gjerne fullkorn</span>
+            </label>
           </li>
           <li data-uid={1}>
-            <span>200</span> <span>g</span> <span>søte cherrytomater</span>{' '}
-            <span>delt i to </span>
+            <label>
+              <input type="checkbox" />
+              <span>200</span> <span>g</span> <span>søte cherrytomater</span>{' '}
+              <span>delt i to </span>
+            </label>
           </li>
           <li data-uid={2}>
-            <span>1 </span> <span>rødløk</span> <span>i halvsirkler</span>
+            <label>
+              <input type="checkbox" />
+              <span>1 </span> <span>rødløk</span> <span>i halvsirkler</span>
+            </label>
           </li>
           <li data-uid={3}>
-            <span>3</span> <span>hvitløksfedd</span> <span>finhakket</span>
+            <label>
+              <input type="checkbox" />
+              <span>3</span> <span>hvitløksfedd</span> <span>finhakket</span>
+            </label>
           </li>
           <li data-uid={4}>
-            <span>100</span> <span>g</span> <span>spinat</span>{' '}
-            <span>grovhakket </span>
+            <label>
+              <input type="checkbox" />
+              <span>100</span> <span>g</span> <span>spinat</span>{' '}
+              <span>grovhakket </span>
+            </label>
           </li>
           <li data-uid={5}>
-            <span>1/2</span> <span>ts</span> <span>chiliflak / 1/2 chili</span>{' '}
-            <span>finhakket</span>
+            <label>
+              <input type="checkbox" />
+              <span>1/2</span> <span>ts</span>{' '}
+              <span>chiliflak / 1/2 chili</span> <span>finhakket</span>
+            </label>
           </li>
           <li data-uid={6}>
-            <span>1</span> <span>ss</span> <span>balsamicoeddik</span>
+            <label>
+              <input type="checkbox" />
+              <span>1</span> <span>ss</span> <span>balsamicoeddik</span>
+            </label>
           </li>
           <li data-uid={7}>
-            <span>1 </span> <span>ss</span> <span>buljongpulver</span>
+            <label>
+              <input type="checkbox" />
+              <span>1 </span> <span>ss</span> <span>buljongpulver</span>
+            </label>
           </li>
           <li data-uid={8}>
-            <span>6</span> <span>dl</span> <span>vann</span>
+            <label>
+              <input type="checkbox" />
+              <span>6</span> <span>dl</span> <span>vann</span>
+            </label>
           </li>
           <li data-uid={9}>
-            <span>2</span> <span>ss</span> <span>olivenolje</span>
+            <label>
+              <input type="checkbox" />
+              <span>2</span> <span>ss</span> <span>olivenolje</span>
+            </label>
           </li>
           <li data-uid={10}>
-            <span>salt og pepper</span>
+            <label>
+              <input type="checkbox" />
+              <span>salt og pepper</span>
+            </label>
           </li>
         </ul>
 
@@ -65,15 +100,27 @@ export default function Home() {
         <h2 className={styles.title}>Fremgangsmåte</h2>
         <ol className={styles.list}>
           <li>
-            Bruk en vid kjele. Brekk gjerne spagettien i to og ha alle
-            ingrediensene, utenom toppingen, i kjelen.
+            <label>
+              <input type="checkbox" />
+              Bruk en vid kjele. Brekk gjerne spagettien i to og ha alle
+              ingrediensene, utenom toppingen, i kjelen.
+            </label>
           </li>
           <li>
-            Kok under jevnlig omrøring i 8–10 minutter til spagettien er al
-            dente og vannet er kokt inn.
+            <label>
+              <input type="checkbox" />
+              Kok under jevnlig omrøring i 8–10 minutter til spagettien er al
+              dente og vannet er kokt inn.
+            </label>
           </li>
-          <li>Smak til med salt og pepper, og topp med ost og basilikum.</li>
+          <li>
+            <label>
+              <input type="checkbox" />
+              Smak til med salt og pepper, og topp med ost og basilikum.
+            </label>
+          </li>
         </ol>
+        <h2 className={styles.title}>Yummy for your tummy!</h2>
       </main>
 
       <footer className={styles.footer}></footer>
